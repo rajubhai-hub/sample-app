94d82f53cc6c645b3d10fa9d3f69c93ceb08026c
rajubhai-hub <bankarajesh2308@gmail.com>
2026-05-21 15:28:58 +0530

    Updated application message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Pipeline Working Successfully"
+    return "New Deployment Triggered Successfully"
 
 @app.route('/health')
 def health():
